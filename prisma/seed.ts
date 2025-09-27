bef2988949ab29025f33cbaa23cf0b0d5b820e2b
henryhoangduong <henryhoangduong@gmail.com>
2025-09-27 14:51:32 +0700

    02: database

---
+++ prisma/seed.ts
@@ -0,0 +1,39 @@
+import {
+  PrismaClient,
+  Prisma,
+  User,
+  Post,
+} from "./../src/generated/prisma/index.d";
+
+const prisma = new PrismaClient();
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: "Alice",
+    email: "alice@prisma.io",
+    posts: {
+      create: [
+        {
+          title: "Join the Prisma Discord",
+          content: "",
+          published: true,
+        },
+      ],
+    },
+  },
+];
+
+export async function main() {
+  for (const u of userData) {
+    await prisma.user.create({ data: u });
+  }
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
